c040cfb097c0e2f050fde71db79a2f2f09cdd9ca
Pavan <pavankt@thoughtworks.com>
2019-01-13 12:01:44 +0530

    Added eslint

--- src/DFA.js
+++ src/DFA.js
@@ -1,31 +1,31 @@
 class DFA {
-  constructor(tuple) {
-    this.states = tuple.states;
-    this.alphabets = tuple.alphabets;
-    this.transitionFunction = tuple.transitionFunction;
-    this.startingState = tuple.startingState;
-    this.acceptableStates = tuple.acceptableStates;
-  }
+	constructor(tuple) {
+		this.states = tuple.states;
+		this.alphabets = tuple.alphabets;
+		this.transitionFunction = tuple.transitionFunction;
+		this.startingState = tuple.startingState;
+		this.acceptableStates = tuple.acceptableStates;
+	}
 
-  validateAlphabet(alphabet) {
-    if (!this.alphabets.includes(alphabet)) {
-      throw `Unknown symbol '${alphabet}'`;
-    }
-  }
+	validateAlphabet(alphabet) {
+		if (!this.alphabets.includes(alphabet)) {
+			throw `Unknown symbol '${alphabet}'`;
+		}
+	}
 
-  changeState(finalState, alphabet) {
-    return this.transitionFunction[finalState][alphabet];
-  }
+	changeState(finalState, alphabet) {
+		return this.transitionFunction[finalState][alphabet];
+	}
 
-  doesAccept(string) {
-    let finalState = this.startingState;
-    string.split("").forEach(alphabet => {
-      this.validateAlphabet(alphabet);
-      finalState = this.changeState(finalState, alphabet);
-    });
+	doesAccept(string) {
+		let finalState = this.startingState;
+		string.split('').forEach(alphabet => {
+			this.validateAlphabet(alphabet);
+			finalState = this.changeState(finalState, alphabet);
+		});
 
-    return this.acceptableStates.includes(finalState);
-  }
+		return this.acceptableStates.includes(finalState);
+	}
 }
 
 module.exports = DFA;
